8fdc49617594e61be63de85abd1d239ce2ee40c8
Raphael de Souza Pimenta <raphael@MacBook-Pro.local>
2020-04-16 16:04:42 -0300

    Testing PORT

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
 export const PORT = ~~process.env.PORT || 3000
-export const API_URL = `https://thawing-earth-73776.herokuapp.com:${PORT}`
+export const API_URL = `https://thawing-earth-73776.herokuapp.com`
